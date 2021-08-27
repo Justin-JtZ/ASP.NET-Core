491daba02ecdd3adc4c23f1de69a0ab9e376a5d7
Justin J <46854715+Justin-JtZ@users.noreply.github.com>
2021-08-27 14:05:07 -0400

    Party-Invite
    Code for Event Hosting Website to save with advanced Reservations

---
+++ PartyInvite/PartyInvite/PartyInvite/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvite.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please Enter A Valid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Phone Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Specify If You'll Attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
